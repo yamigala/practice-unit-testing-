5470c8bcddb7e853227827cf6cbac68d3016f42e
Yamini Gala <yamini.gala@1rivet.local>
2019-01-15 20:00:47 +0530

    feat: triggering event on element

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -8,11 +8,11 @@ import { By } from "@angular/platform-browser";
 
 describe('HeroesComponent (deep tests)', () => {
     let fixture: ComponentFixture<HeroesComponent>;
-    let HEROS;
+    let HEROES;
     let mockHeroService;
 
     beforeEach(() => {
-        HEROS = [
+        HEROES = [
             { id: 1, name: 'scsx', strength: 33 },
             { id: 2, name: 'wedf', strength: 44 }
         ]
@@ -31,13 +31,24 @@ describe('HeroesComponent (deep tests)', () => {
     })
 
     it('should render each hero as a HeroComponent', () => {
-        mockHeroService.getHeroes.and.returnValue(of(HEROS));
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
 
         fixture.detectChanges();
         const heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
         expect(heroComponentDEs.length).toEqual(2);
         for (let i = 0; i < heroComponentDEs.length; i++) {
-            expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROS[i]);
+            expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROES[i]);
         }
     })
+    it(`should call heroService .deletehero when hero component's
+        delete button is clicked `, ()=>{
+            spyOn(fixture.componentInstance, 'delete');
+            mockHeroService.getHeroes.and.returnValue(of(HEROES));
+            fixture.detectChanges();
+
+            const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+            heroComponents[1].query(By.css('button'))
+            .triggerEventHandler('click', {stopPropagation: ()=>{}});
+            expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[1]);
+        })
 })
